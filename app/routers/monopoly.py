dc838ac2528772f1d5c25bede63238b517e0fa8c
christian <christianmoryah@gmail.com>
2021-01-16 19:38:21 -0300

    finaliza interface api v0

--- app/routers/monopoly.py
+++ app/routers/monopoly.py
@@ -4,9 +4,9 @@ from fastapi import Query
 
 import logging
 
-from models.response_models import InitGameReturn
+from models.response_models import GameStateResponse, RunGameResponse
 
-from models.requests_models import InitGameRequest
+from models.requests_models import InitGameRequest, ProcessRoundRequest, RunGameRequest
 
 import main
 
@@ -27,9 +27,9 @@ logger.addHandler(fh)
 logger.addHandler(ch)
 
 
-@router.post('/new_game', response_model=InitGameReturn)
-async def fixation_sequence(init_game_request: InitGameRequest):
-	"""Processa e retorna a imagem original com identificação da sequência de fixação."""
+@router.post('/new_game', response_model=GameStateResponse)
+async def new_game(init_game_request: InitGameRequest):
+	"""Inicializa um novo jogo, reinicia todos os valores"""
 
 	random_seed = init_game_request.random_seed
 
@@ -41,4 +41,26 @@ async def fixation_sequence(init_game_request: InitGameRequest):
 	players_list_return = players_list.copy()
 	players_list_return.pop(0)
 
-	return InitGameReturn(players_list=players_list_return, players_order=players_order, board=board)
+	return GameStateResponse(players_list=players_list_return, players_order=players_order, board=board)
+
+@router.post('/process_round', response_model=GameStateResponse)
+async def process_round(process_round_request: ProcessRoundRequest):
+	"""Processa uma rodada e retorna o estado atual do jogo"""
+	
+	random_seed = process_round_request.random_seed
+
+	players_list, players_order, board = main.game_instance.process_single_round(random_seed)
+	players_list_return = players_list.copy()
+	players_list_return.pop(0)
+
+	return GameStateResponse(players_list=players_list_return, players_order=players_order, board=board)
+
+@router.post('/run_game', response_model=RunGameResponse)
+async def run_game(run_game_request: RunGameRequest):
+	"""Executa um jogo inteiro, retorna o vencedor e o número de partidas."""
+	
+	random_seed = run_game_request.random_seed
+
+	winner, round_ = main.game_instance.run_game(random_seed)
+
+	return RunGameResponse(winner=winner, round_=round_)
